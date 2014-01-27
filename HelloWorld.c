712714f23a2cf0c2836b47451d9e5bdddf4b3b00
Codernova <travisbeckstrand@comcast.net>
2014-01-26 19:35:14 -0700

    Hello World version 2

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,6 +1,13 @@
+// HelloWorld.c
+
+// Programming assignment #3 for CS-2450
+// Professor: Craig Gardner
+// Student and author: Travis Beckstrand
+// Recreate the "Hello, World!" program
 #include <stdio.h>
 
 int main()
 {
-	printf("Hello, World!");
+	printf("Hello, World!\n");
+	return 0;
 }
